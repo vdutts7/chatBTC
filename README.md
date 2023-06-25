90de7453dbcb31c84bb13f72ec9d0d867b458efe
ViveK Dutta <vivekd@berkeley.edu>
2023-06-25 10:38:30 -0700

    new

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# chatBTC
-
-![_btc](https://github.com/vdutts7/chatBTC/assets/63992417/22a28076-f96a-4757-98a6-87eef4a30225)
+# BTC
--- utils.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def get_base64(file_path):
-    with open(file_path, "rb") as f:
-        contents = f.read()
-        encoded = base64.b64encode(contents).decode()
-    return encoded
